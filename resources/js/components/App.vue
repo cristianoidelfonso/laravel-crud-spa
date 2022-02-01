fa40c3050fd8dfc72a13aec2c0b4314d716f6c10
Cristiano Idelfonso <cristiano_idelfonso@hotmail.com>
2022-01-31 21:10:48 -0300

    first commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <main>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <router-link to="/" class="navbar-brand" href="#">Laravel Vue Crud App</router-link>
+                <div class="collapse navbar-collapse">
+                    <div class="navbar-nav">
+                        <router-link exact-active-class="active" to="/" class="nav-item nav-link">Home</router-link>
+                        <router-link exact-active-class="active" to="/category" class="nav-item nav-link">Category</router-link>
+                        <a href="https://techvblogs.com/blog/build-crud-app-with-laravel-and-vuejs" class="nav-item nav-link" target="_blank">Referência</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="container mt-5">
+            <router-view></router-view>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {}
+</script>
---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="container mt-5">
+        <div class="col-12 text-center">
+
+        </div>
+    </div>
+</template>
